4694776f49383999d801f1b0e7a4709286078739
Masonlet <masonletoile@hotmail.com>
2026-07-23 14:31:20 -0400

    chore: extract PORTFOLIO const

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ interface RewriteRule {
   prefix?:     string;
   destination: string;
 }
+const rewrites: RewriteRule[] = [
+  { source: '/api/languages',    destination: 'https://github-top-languages.vercel.app/api/languages' },
+  { source: '/api/contact',      destination: 'https://contact-api-resend.vercel.app/api/contact' 		},
+  { source: '/api/readme',       destination: 'https://readme-api-nine.vercel.app/api/readme' 				},
+  { prefix: '/contact-api-demo', destination: 'https://contact-api-demo.pages.dev' 									  },
+  { prefix: '/readme-api-demo',  destination: 'https://readme-api-demo.pages.dev' 										},
+  { prefix: '/pixel-parker',     destination: 'https://pixel-parker.pages.dev' 												},
+  { prefix: '/starweb-sandbox',  destination: 'https://starweb-sandbox.pages.dev' 										},
+  { prefix: '/gh-top-languages-builder', destination: 'https://gh-top-languages-builder.pages.dev'    }
+];
+const PORTFOLIO = 'https://portfolio-8dg.pages.dev';
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url);
 
     if (url.hostname === 'masonletoile.com' || url.hostname === 'www.masonletoile.com') {
@@ -14,17 +25,6 @@ export default {
     }
 
     const pathname = url.pathname;
-
-    const rewrites: RewriteRule[] = [
-      { source: '/api/languages',    destination: 'https://github-top-languages.vercel.app/api/languages' },
-      { source: '/api/contact',      destination: 'https://contact-api-resend.vercel.app/api/contact' 		},
-      { source: '/api/readme',       destination: 'https://readme-api-nine.vercel.app/api/readme' 				},
-      { prefix: '/contact-api-demo', destination: 'https://contact-api-demo.pages.dev' 									  },
-      { prefix: '/readme-api-demo',  destination: 'https://readme-api-demo.pages.dev' 										},
-      { prefix: '/pixel-parker',     destination: 'https://pixel-parker.pages.dev' 												},
-      { prefix: '/starweb-sandbox',  destination: 'https://starweb-sandbox.pages.dev' 										},
-      { prefix: '/gh-top-languages-builder', destination: 'https://gh-top-languages-builder.pages.dev'    }
-    ];
 
     for (const rule of rewrites) if (rule.source && pathname === rule.source) return fetch(
 			`${rule.destination}${url.search}`, request
@@ -44,6 +44,6 @@ export default {
       }
     }
 
-    return fetch(`https://portfolio-8dg.pages.dev${url.pathname}${url.search}`, request);
+    return fetch(`${PORTFOLIO}${url.pathname}${url.search}`, request);
   }
 } satisfies ExportedHandler<Env>;
